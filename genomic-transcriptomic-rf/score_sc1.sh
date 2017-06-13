04b15051df1a56c46076888ad2886e3b8fde6dd3
Brian White <brian.white@sagebase.org>
2017-06-13 10:32:06 -0700

    Removed all old images that used deprecated file organization

--- genomic-transcriptomic-rf/score_sc1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-metadata_dir=/metadata
-test_dir=/test-data
-output_dir=/output
-
-./run-mm-sc1.R --metadata-dir=$metadata_dir --test-dir=$test_dir --output-dir=$output_dir
--- uams-5/score_sc1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-test_dir=/test-data
-output_dir=/output
-
-./run-mm-sc1.R --test-dir=$test_dir --output-dir=$output_dir
